98e0ca5badb23728f5ef0ec3533abfb4681b4a53
jana <bkeyh382@gmail.com>
2026-06-24 09:43:25 +0300

    Refactor print_dlistint function for clarity

--- challenge/4-delete_dnodeint/print_dlistint.c
+++ challenge/4-delete_dnodeint/print_dlistint.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
 #include "lists.h"
+
 /**
- * print_dlistint - Prints a doubly linkedlist of integers
- *
- * @h: A pointer to the first element of a list
- *
- * Return: The number of element printed
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t n;
-	n = 0;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		n++;
-	}
-	return (n);
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        count++;
+    }
+    return (count);
 }
