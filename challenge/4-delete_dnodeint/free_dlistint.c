daee0def3ab87fae028f2f4373b48cf6a62bc3d0
jana <bkeyh382@gmail.com>
2026-06-24 16:04:30 +0300

    Refactor free_dlistint for improved readability
    Updated the free_dlistint function to improve clarity and consistency in the code.

--- challenge/4-delete_dnodeint/free_dlistint.c
+++ challenge/4-delete_dnodeint/free_dlistint.c
@@ -1,18 +1,20 @@
-#include "lists.h"
+#include <string.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * free_dlistint - frees a dlistint_t list
- * @head: head of the list
+ * free_dlistint - Free a list
+ *
+ * @head: A pointer to the first element of the list
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *tmp;
+	dlistint_t *node;
 
-	while (head != NULL)
+	while (head)
 	{
-		tmp = head;
+		node = head;
 		head = head->next;
-		free(tmp);
+		free(node);
 	}
 }
