cf37b270d10452cf02ce9b265cf2303d8f638058
jana <bkeyh382@gmail.com>
2026-06-24 09:42:55 +0300

    Refactor lists.h for header guard and documentation
    Updated header guard and comments in lists.h

--- challenge/4-delete_dnodeint/lists.h
+++ challenge/4-delete_dnodeint/lists.h
@@ -1,16 +1,17 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
- * @prev: points to the previous node
+ * @prev: points to the prev node
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
@@ -20,13 +21,8 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-void free_dlistint(dlistint_t *head);
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
-int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+void free_dlistint(dlistint_t *head);
 
-#endif /* LISTS_H */
+#endif /* _LISTS_H_ */
